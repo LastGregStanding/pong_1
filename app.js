2154c8b1e5d2bdc63d1a1353623ac6c17f26723f
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-07-05 10:10:29 -0700

    added player movement

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const grid = document.querySelector('.grid');
 const squares = [];
-const width = 50;
+const width = 51;
 
 const createBoard = function () {
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 1020; i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
@@ -18,15 +18,70 @@ createBoard();
 const player1 = [0, width, width * 2, width * 3];
 const player2 = [0, width, width * 2, width * 3];
 
-let player1Index = 150;
-let player2Index = 149;
+// Draw net
+const net = [];
+for (let i = 0; i < 19; i++) {
+    i++;
+    let square = (width * i) + 26;
+    squares[square].classList.add('net');
+    net.push(square);
+}
 
+
+let player1Index = 357;
+let player2Index = 407;
+
+
+//#region Draw/UnDraw Players
 const drawPlayer1 = function () {
     player1.forEach((e) => squares[e + player1Index].classList.add('player'))
+}
+const undrawPlayer1 = function () {
+    player1.forEach((e) => squares[e + player1Index].classList.remove('player'))
 }
 const drawPlayer2 = function () {
     player1.forEach((e) => squares[e + player2Index].classList.add('player'))
 }
+const undrawPlayer2 = function () {
+    player1.forEach((e) => squares[e + player2Index].classList.remove('player'))
+}
+//#endregion
 
 drawPlayer1();
 drawPlayer2();
+
+document.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+
+        //player2 Move Up
+        case 38:
+            undrawPlayer2();
+            player2Index -= width;
+            drawPlayer2();
+            break;
+
+        //player2 Move Down
+        case 40:
+            undrawPlayer2();
+            player2Index += width;
+            drawPlayer2();
+            break;
+
+        //player1 Move Up
+        case 87:
+            undrawPlayer1();
+            player1Index -= width;
+            drawPlayer1();
+            break;
+
+        //player1 Move Down
+        case 83:
+            undrawPlayer1();
+            player1Index += width;
+            drawPlayer1();
+            break;
+
+    }
+
+
+})
